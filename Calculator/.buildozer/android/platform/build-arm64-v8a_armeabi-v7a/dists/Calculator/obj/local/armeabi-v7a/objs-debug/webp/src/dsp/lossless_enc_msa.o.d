43be28918bf642da69cfb918a1c8e349c02e60b0
SahilS8877 <71732134+Sahil8877@users.noreply.github.com>
2025-10-20 00:17:01 +0100

    Delete Calculator/.buildozer directory

--- Calculator/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/Calculator/obj/local/armeabi-v7a/objs-debug/webp/src/dsp/lossless_enc_msa.o.d
+++
@@ -1,8 +0,0 @@
-/Users/sahilsuryavanshi/Files/CodeKarBhai/Python/Kivy/Calculator/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/dsp/lossless_enc_msa.o: \
-  /Users/sahilsuryavanshi/Files/CodeKarBhai/Python/Kivy/Calculator/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/lossless_enc_msa.c \
-  /Users/sahilsuryavanshi/Files/CodeKarBhai/Python/Kivy/Calculator/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h \
-  /Users/sahilsuryavanshi/Files/CodeKarBhai/Python/Kivy/Calculator/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h
-
-/Users/sahilsuryavanshi/Files/CodeKarBhai/Python/Kivy/Calculator/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h:
-
-/Users/sahilsuryavanshi/Files/CodeKarBhai/Python/Kivy/Calculator/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h:
